2a91dbb619277f69814aa18eed2ebe8bce06a9d5
Mathias Rieder <mathias.rieder@gmail.com>
2018-04-10 21:06:41 +0200

    initial import

---
+++ UpperCase.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * turns the given String into upper-case characters
+*/
+void toUpperCase(char* text){
+  //TODO
+}
+
+void test(char* text){
+  char* newString = malloc(strlen(text)*sizeof(char)+1);
+  strcpy(newString, text);
+  toUpperCase(newString);
+  printf("%s --> %s\n", text, newString);
+}
+
+
+int main(int argc, char const *argv[]) {
+  test("abc");
+  test("hello World");
+  test("");
+  test("1 two 3 four five 6");
+  return 0;
+}
